8275a8bba9140bb8f0ae68320b0b0636aee8d8cd
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-03-28 22:22:05 +0700

    Add new data to POST to web app

--- drone/mission.py
+++ drone/mission.py
@@ -41,6 +41,13 @@ def arm_and_takeoff(aTargetAltitude):
             "gps_longitude": vehicle.location.global_relative_frame.lon,
             "altitude": vehicle.location.global_relative_frame.alt,
             "battery_voltage": vehicle.battery.voltage,
+            "battery_level": vehicle.battery.level,
+            "battery_current": vehicle.battery.current,
+            "ekf_ok": vehicle.ekf_ok,
+            "is_armable": vehicle.is_armable,
+            "system_status": vehicle.system_status.state,
+            "mode": vehicle.mode.name,
+            "armed": vehicle.armed,
             "drone_id": drone_id,
         }
 
