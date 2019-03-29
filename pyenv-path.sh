2969cf13594809b29aa205aac4ecfa31f3128941
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-03-29 12:45:34 +0700

    Fix env

--- pyenv-path.sh
+++ pyenv-path.sh
@@ -11,5 +11,5 @@ if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
 
-export PYTHONPATH="${PYTHONPATH}:/home/adam/drone/mavlink"
-export PATH=/home/adam/.pyenv/shims:/home/adam/.pyenv/bin:/home/adam/.rbenv/plugins/ruby-build/bin:/home/adam/.rbenv/shims:/home/adam/.rbenv/bin:/home/adam/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/adam/.local/bin
+export PYTHONPATH="${PYTHONPATH}:/home/pi/drone/mavlink"
+export PATH=/home/pi/.pyenv/shims:/home/pi/.pyenv/bin:/home/pi/.rbenv/plugins/ruby-build/bin:/home/pi/.rbenv/shims:/home/pi/.rbenv/bin:/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/pi/.local/bin
