145ba4df1ede704576c5b8ce5f51325ff5c32490
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-03-29 12:43:30 +0700

    Fix bash script for env

--- pyenv-path.sh
+++ pyenv-path.sh
@@ -2,7 +2,7 @@
 # Always source this file when running commands remotely with 'source pyenv-path.sh'
 
 export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+# eval "$(rbenv init -)"
 export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
