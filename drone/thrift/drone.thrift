7afb7953d93709698eb8524d45af42f9affa6f30
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-04-08 15:37:22 +0700

    Add new thrift system for drone control

---
+++ drone/thrift/drone.thrift
@@ -0,0 +1,16 @@
+namespace cpp base
+namespace d base
+namespace dart base
+namespace java base
+namespace php base
+namespace perl base
+namespace haxe base
+namespace netcore base
+
+service Drone{
+    void takeoff(1:double altitude),
+    
+    void land(),
+
+    void fly_to(1:double latitude, 2:double longitude)
+}
---
+++ drone/thrift/server.py
@@ -0,0 +1,66 @@
+# Thrift and py imports
+import glob
+import sys
+import time
+
+sys.path.append('gen-py')
+
+from drone import Drone
+
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+
+# Dronekit imports
+from dronekit import connect, VehicleMode, Command, LocationGlobalRelative
+from pymavlink import mavutil
+
+class DroneHandler:
+    def __init__(self):
+        self.log = {}
+        self.vehicle = connect("udp:127.0.0.1:14551", wait_ready=True)
+    
+    def takeoff(self, alt):
+        print "Basic pre-arm checks"
+        # Don't try to arm until autopilot is ready
+        while not self.vehicle.is_armable:
+            print "Waiting for vehicle to initialise..."
+            time.sleep(1)
+
+        print "Arming motors"
+        # Copter should arm in GUIDED mode
+        self.vehicle.mode    = VehicleMode("GUIDED")
+        self.vehicle.armed   = True
+
+        # Confirm vehicle armed before attempting to take off
+        while not self.vehicle.armed:
+            print " Waiting for arming..."
+            time.sleep(1)
+
+        print "Taking off!"
+        self.vehicle.simple_takeoff(alt) # Take off to target altitude
+
+    def land(self):
+        self.vehicle.mode = VehicleMode("RTL")
+
+    def fly_to(self, lat, lng):
+        if self.vehicle.mode != VehicleMode("GUIDED"):
+            self.vehicle.mode = VehicleMode("GUIDED")  
+
+        location = LocationGlobalRelative(lat, lng, 40)
+        self.vehicle.simple_goto(location)
+
+
+if __name__ == '__main__':
+    handler = DroneHandler()
+    processor = Drone.Processor(handler)
+    transport = TSocket.TServerSocket(host='0.0.0.0', port=9090)
+    tfactory = TTransport.TBufferedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
+    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+
+    print('Starting the server...')
+    server.serve()
+    print('done.')
