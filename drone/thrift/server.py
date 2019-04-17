451b2897beb443de25e6531ee565b640cc2d2d84
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-04-17 18:25:05 +0700

    Removed home location

--- drone/thrift/server.py
+++ drone/thrift/server.py
@@ -61,11 +61,11 @@ class DroneHandler:
 
         self.download_missions()
 
-        self.fixed_home_location = {
-            "latitude": self.vehicle.home_location.lat,
-            "longitude": self.vehicle.home_location.lon,
-        }
-        print("First home location: {0},{1}".format(self.fixed_home_location["latitude"],self.fixed_home_location["longitude"]))
+        # self.fixed_home_location = {
+        #     "latitude": self.vehicle.home_location.lat,
+        #     "longitude": self.vehicle.home_location.lon,
+        # }
+        # print("First home location: {0},{1}".format(self.fixed_home_location["latitude"],self.fixed_home_location["longitude"]))
 
         self.report_status(drone_id)
 
