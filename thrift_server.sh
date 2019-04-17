cadedbf924c2e3c0c340a5729c3a3bfb6843a5ce
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-04-17 12:00:21 +0700

    Add path to thrift serve script

--- thrift_server.sh
+++ thrift_server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+PATH=/home/pi/.pyenv/shims:/home/pi/.pyenv/bin:/home/pi/.rbenv/plugins/ruby-build/bin:/home/pi/.rbenv/shims:/home/pi/.rbenv/bin:/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/pi/.local/bin
+
 python ~/drone-comms/drone/thrift/server.py udp:192.168.4.2:14550 --user pi --drone_id 2
