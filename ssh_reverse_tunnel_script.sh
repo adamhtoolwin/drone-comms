063b13a7f8c84c3c72e60b9b41f139123b8be350
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-04-17 12:12:49 +0700

    Add path to ssh script

--- ssh_reverse_tunnel_script.sh
+++ ssh_reverse_tunnel_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SHELL=/bin/sh PATH=/bin:/sbin:/usr/bin:/usr/sbin
+SHELL=/bin/bash PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 ssh -i ~/dronetracker.pem -f -N -T -R 2019:localhost:22 webserver-aws
 ssh -i ~/dronetracker.pem -f -N -T -R 9091:localhost:9090 webserver-aws
