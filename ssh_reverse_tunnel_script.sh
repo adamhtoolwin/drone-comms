6b09f0a8adb88604291d6bdedaf8e9aec349001a
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-05-01 22:15:29 +0700

    Fixed path in ssh script

--- ssh_reverse_tunnel_script.sh
+++ ssh_reverse_tunnel_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SHELL=/bin/bash PATH=/bin:/sbin:/usr/bin:/usr/sbin
+PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
-ssh -i ~/dronetracker.pem -f -N -T -R 2019:localhost:22 webserver-aws
-ssh -i ~/dronetracker.pem -f -N -T -R 9091:localhost:9090 webserver-aws
+ssh -i /home/pi/dronetracker.pem -f -N -T -R 2019:localhost:22 webserver-aws
+ssh -i /home/pi/dronetracker.pem -f -N -T -R 9091:localhost:9090 webserver-aws
 
